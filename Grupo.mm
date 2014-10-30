5ab070dd689799700fbc1148053f165229383852
chelizalde <spunner.napster@yahoo.com.mx>
2014-10-29 23:48:48 -0600

    se agrega grupos.mm

---
+++ Grupo.mm
@@ -0,0 +1,7 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1414647024388" ID="ID_1620677343" MODIFIED="1414647044654" TEXT="Grupo">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+</node>
+</map>
